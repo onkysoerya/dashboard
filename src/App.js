0bce0a6b54d19d6f41c5a3291b576563933a24d8
unknown <onkysoerya@gmail.com>
2019-03-26 09:27:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import DashboardAdmin from './View/DashboardAdmin';
 import Home from './View/Home';
+import TambahBerita from './View/TambahBerita';
 
 class App extends Component {
   render(){
     return (
       <div className="">
-      <DashboardAdmin/>
         <Switch>
-          <Route excat path="/" Component={Home}/>
-          <Route path="/dashboard" Component={DashboardAdmin}/>
+          <Route exact path="/" component={Home}/>
+          <Route path="/dashboard" component={DashboardAdmin}/>
+          <Route path="/tambahberita" component={TambahBerita}/>
         </Switch>
       </div>
     );
